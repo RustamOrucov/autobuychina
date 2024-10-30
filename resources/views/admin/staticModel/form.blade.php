1f53de900655f0b117e55d7975d7a23108a7a14f
Telman00 <inustut@gmail.com>
2024-10-30 12:56:19 +0400

    Static

--- resources/views/admin/staticModel/form.blade.php
+++ resources/views/admin/staticModel/form.blade.php
@@ -31,37 +31,37 @@
                             <div class="tab-pane fade {{ $loop->first ? 'active show' : '' }}" id="tab-{{ $lang }}" role="tabpanel" aria-labelledby="custom-tabs-one-{{ $lang }}-tab">
                                 <div class="row g-3">
                                     <div class="col-12 col-lg-6">
-                                        <label for="Title" class="form-label">Static 1</label>
-                                        <input value="{{ old($lang.'title', isset($model->translateOrDefault($lang)->title) ? $model->translateOrDefault($lang)->title : '') }}" type="text" class="form-control" id="Title" placeholder="Static 1" name="{{ $lang }}[title]">
-                                        @error("$lang.title")
-                                        <span class="text-danger">{{ $message }}</span>
-                                        @enderror
-                                    </div>
-                                    <div class="col-12 col-lg-6">
-                                        <label for="text" class="form-label">Static 2</label>
-                                        <input value="{{ old($lang.'text', isset($model->translateOrDefault($lang)->text) ? $model->translateOrDefault($lang)->text : '') }}" type="text" class="form-control" id="text" placeholder="Static 2" name="{{ $lang }}[text]">
-                                        @error("$lang.text")
-                                        <span class="text-danger">{{ $message }}</span>
-                                        @enderror
-                                    </div>
-                                    <div class="col-12 col-lg-6">
-                                        <label for="desc" class="form-label">Static 3</label>
-                                        <input value="{{ old($lang.'desc', isset($model->translateOrDefault($lang)->desc) ? $model->translateOrDefault($lang)->desc : '') }}" type="text" class="form-control" id="desc" placeholder="Static 3" name="{{ $lang }}[desc]">
-                                        @error("$lang.desc")
-                                        <span class="text-danger">{{ $message }}</span>
-                                        @enderror
-                                    </div>
-                                    <div class="col-12 col-lg-6">
-                                        <label for="Name" class="form-label">Static 4</label>
-                                        <input value="{{ old($lang.'name', isset($model->translateOrDefault($lang)->name) ? $model->translateOrDefault($lang)->name : '') }}" type="text" class="form-control" id="Name" placeholder="Static 4" name="{{ $lang }}[name]">
-                                        @error("$lang.name")
-                                        <span class="text-danger">{{ $message }}</span>
-                                        @enderror
-                                    </div>
-                                </div>
-                            </div>
-                        @endforeach
-                    </div>
+    <label for="Title" class="form-label">Static 1</label>
+    <input value="{{ old($lang.'title', isset($model) ? optional($model->translateOrDefault($lang))->title : '') }}" type="text" class="form-control" id="Title" placeholder="Static 1" name="{{ $lang }}[title]">
+    @error("$lang.title")
+    <span class="text-danger">{{ $message }}</span>
+    @enderror
+</div>
+<div class="col-12 col-lg-6">
+    <label for="text" class="form-label">Static 2</label>
+    <input value="{{ old($lang.'text', isset($model) ? optional($model->translateOrDefault($lang))->text : '') }}" type="text" class="form-control" id="text" placeholder="Static 2" name="{{ $lang }}[text]">
+    @error("$lang.text")
+    <span class="text-danger">{{ $message }}</span>
+    @enderror
+</div>
+<div class="col-12 col-lg-6">
+    <label for="desc" class="form-label">Static 3</label>
+    <input value="{{ old($lang.'desc', isset($model) ? optional($model->translateOrDefault($lang))->desc : '') }}" type="text" class="form-control" id="desc" placeholder="Static 3" name="{{ $lang }}[desc]">
+    @error("$lang.desc")
+    <span class="text-danger">{{ $message }}</span>
+    @enderror
+</div>
+<div class="col-12 col-lg-6">
+    <label for="Name" class="form-label">Static 4</label>
+    <input value="{{ old($lang.'name', isset($model) ? optional($model->translateOrDefault($lang))->name : '') }}" type="text" class="form-control" id="Name" placeholder="Static 4" name="{{ $lang }}[name]">
+    @error("$lang.name")
+    <span class="text-danger">{{ $message }}</span>
+    @enderror
+</div>
+</div>
+</div>
+@endforeach
+</div>
                     <div class="form-group">
                         <label>Key</label>
                         <input  @if(isset($model)) readonly @endif type="text" placeholder="Key" name="key" value="{{old('key',$model->key ?? '')}}" class="form-control" @isset($model) readonly @endisset>
