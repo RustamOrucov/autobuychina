a8d4548a65435212b78f83717d1b7897282b89c5
Telman00 <inustut@gmail.com>
2024-10-31 13:03:22 +0400

    static_mobile

--- resources/views/mobile/pages/rules.blade.php
+++ resources/views/mobile/pages/rules.blade.php
@@ -5,14 +5,14 @@
             <div class="header__nav header__nav--no-back-btn">
                 <div class="header__nav-left no-flex"></div>
                 <div class="header__nav-title">
-                    <h1 class="section-title_name">Rules</h1>
+                    <h1 class="section-title_name">{{ sitekey('list_car_eight', 'name') }}</h1>
                 </div>
-                <div class="header__nav-right"><a class="header__logo header__logo--red" href="/">AutoBuyChina</a></div>
+                <div class="header__nav-right"><a class="header__logo header__logo--red" href="/">{{ sitekey('detail_car', 'title') }}</a></div>
             </div>
         </div>
         <div class="content">
             <div class="md-content">
-                <h4>General Rules</h4>
+                <h4>{{ sitekey('register_mobile', 'text') }}</h4>
                 <p>
                     @foreach($policys as $policy)
                     {!! $policy->agreement !!}
